90d38701b9373c5fb875520177aa2837b45f7b11
Trinatha kumar sethy <trinathsethy872@gmail.com>
2024-02-12 10:28:38 +0530

    PoojaCategory Crud Operation

--- AdminApi/Controllers/PoojaCategoryController.cs
+++ AdminApi/Controllers/PoojaCategoryController.cs
@@ -1,9 +1,144 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AdminApi.DTO.App.LocationDTO;
+using AdminApi.Models;
+using AdminApi.Models.App.Location;
+using AdminApi.Models.App.PoojaCategory;
+using AdminApi.Models.Helper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+using System;
+using AdminApi.DTO.App.PoojaCategoryDTO;
 
 namespace AdminApi.Controllers
 {
-	public class PoojaCategoryController : Controller
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PoojaCategoryController : Controller
 	{
-		
-	}
+        private readonly IConfiguration _config;
+        private readonly AppDbContext _context;
+        private readonly ISqlRepository<PoojaCategory> _PoojaCategoryRepo;
+
+        public PoojaCategoryController(IConfiguration config,
+                                 AppDbContext context,
+                                 ISqlRepository<PoojaCategory> poojaCategoryRepo)
+        {
+            _config = config;
+            _context = context;
+            _PoojaCategoryRepo = poojaCategoryRepo;
+        }
+        [HttpPost]
+        public IActionResult PoojaCategoryCreate(CreatePoojaCategoryDTO createPoojaCategoryDTO)
+        {
+            var objcheck = _context.PoojaCategories.SingleOrDefault(opt => opt.PoojaCategoryName == createPoojaCategoryDTO.PoojaCategoryName && opt.IsDeleted == false);
+            try
+            {
+                if (objcheck == null)
+                {
+                    PoojaCategory poojaCategory = new PoojaCategory();
+
+                    poojaCategory.PoojaCategoryName = createPoojaCategoryDTO.PoojaCategoryName;
+
+                    poojaCategory.CreatedBy = createPoojaCategoryDTO.CreatedBy;
+                    poojaCategory.CreatedOn = System.DateTime.Now;
+                    var obj = _PoojaCategoryRepo.Insert(poojaCategory);
+                    return Ok(obj);
+                }
+                else if (objcheck != null)
+                {
+                    return Accepted(new Confirmation { Status = "Duplicate", ResponseMsg = "Duplicate CountryName..!" });
+                }
+                return Accepted(new Confirmation { Status = "Error", ResponseMsg = "Something unexpected!" });
+            }
+            catch (Exception ex)
+            {
+                return Accepted(new Confirmation { Status = "error", ResponseMsg = ex.Message });
+            }
+
+        }
+
+
+        [HttpPost]
+        public ActionResult UpdatePoojaCategory(UpdatePoojaCategoryDTO updatePoojaCategoryDTO)
+        {
+            try
+            {
+                var objPoojaCategory = _context.PoojaCategories.SingleOrDefault(opt => opt.PoojaCategoryId == updatePoojaCategoryDTO.PoojaCategoryId);
+
+                var existingPoojaCategory = _context.PoojaCategories.SingleOrDefault(opt => opt.PoojaCategoryName == updatePoojaCategoryDTO.PoojaCategoryName && opt.PoojaCategoryId != updatePoojaCategoryDTO.PoojaCategoryId && opt.IsDeleted == false);
+
+                if (existingPoojaCategory != null)
+                {
+                    return Accepted(new Confirmation { Status = "Duplicate", ResponseMsg = "Duplicate CountryName..!" });
+                }
+
+                objPoojaCategory.PoojaCategoryName = updatePoojaCategoryDTO.PoojaCategoryName;
+                objPoojaCategory.UpdatedBy = updatePoojaCategoryDTO.CreatedBy;
+                objPoojaCategory.UpdatedOn = System.DateTime.Now;
+                _context.SaveChanges();
+                return Ok(objPoojaCategory);
+            }
+            catch (Exception ex)
+            {
+                return Accepted(new Confirmation { Status = "error", ResponseMsg = ex.Message });
+            }
+        }
+        [HttpGet]
+        public ActionResult GetPoojaCategoryList()
+        {
+            try
+            {
+                var list = (from u in _context.PoojaCategories
+
+                            select new
+                            {
+                                u.PoojaCategoryName,
+                                u.PoojaCategoryId,
+                                u.IsDeleted
+                            }).Where(x => x.IsDeleted == false).ToList();
+
+                int totalRecords = list.Count();
+
+                return Ok(new { data = list, recordsTotal = totalRecords, recordsFiltered = totalRecords });
+            }
+            catch (Exception ex)
+            {
+                return Accepted(new Confirmation { Status = "error", ResponseMsg = ex.Message });
+            }
+        }
+
+
+        [HttpGet("{PoojaCategoryId}")]
+        public ActionResult GetSinglePoojaCategory(int PoojaCategoryId)
+        {
+            try
+            {
+                var singleCountry = _PoojaCategoryRepo.SelectById(PoojaCategoryId);
+                return Ok(singleCountry);
+            }
+            catch (Exception ex)
+            {
+                return Accepted(new Confirmation { Status = "error", ResponseMsg = ex.Message });
+            }
+        }
+
+
+        [HttpGet("{Id}/{DeletedBy}")]
+        public ActionResult DeletePoojaCategory(int Id, int DeletedBy)
+        {
+            try
+            {
+                var objabout = _context.PoojaCategories.SingleOrDefault(opt => opt.PoojaCategoryId == Id);
+                objabout.IsDeleted = true;
+                objabout.UpdatedBy = DeletedBy;
+                objabout.UpdatedOn = System.DateTime.Now;
+                _context.SaveChanges();
+                return Ok(objabout);
+            }
+            catch (Exception ex)
+            {
+                return Accepted(new Confirmation { Status = "error", ResponseMsg = ex.Message });
+            }
+        }
+    }
 }
---
+++ AdminClient/Controllers/PoojaCategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminClient.Controllers
+{
+    public class PoojaCategoryController : Controller
+    {
+        public IActionResult CreatePoojaCategory()
+        {
+            return View();
+        }
+        public IActionResult PoojaCategoryList()
+        {
+            return View();
+        }
+        public IActionResult PoojaCategoryEdit(int PoojaCategoryId)
+        {
+            ViewBag.PoojaCategoryId = PoojaCategoryId;
+            return View();
+        }
+    }
+}
