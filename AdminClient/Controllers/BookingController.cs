0de5f7cdc484db2d9a7b5293ecd1352f3a2ba212
Trinatha kumar sethy <trinathsethy872@gmail.com>
2024-02-20 15:47:53 +0530

    Booking Crud

---
+++ AdminClient/Controllers/BookingController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminClient.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult CreateBooking()
+        {
+            return View();
+        }
+        public IActionResult BookingList()
+        {
+            return View();
+        }
+        public IActionResult BookingEdit(int BookingId)
+        {
+            ViewBag.BookingId = BookingId;
+            return View();
+        }
+    }
+}
