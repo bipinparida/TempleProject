ea4c96a037ab95bddc22acf56b3b2fceeeb107fa
Bipin Bihari Parida <bipinparida111@gmail.com>
2024-02-21 18:00:37 +0530

    Pooja CategoryType Add

---
+++ AdminApi/DTO/App/PoojaCategoryDTO/CreatePoojaCategoryTypeDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace AdminApi.DTO.App.PoojaCategoryDTO
+{
+    public class CreatePoojaCategoryTypeDTO
+    {
+        public string PoojaCategoryTypeName { get; set; }
+
+        public int CreatedBy { get; set; }
+    }
+
+    public class UpdatePoojaCategoryTypeDTO
+    {
+        public int PoojaCategoryTypeId { get; set; }
+        public string PoojaCategoryTypeName { get; set; }
+
+        public int CreatedBy { get; set; }
+
+    }
+
+}
---
+++ AdminApi/Models/App/PoojaCategory/PoojaCategoryType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdminApi.Models.App.PoojaCategory
+{
+    public class PoojaCategoryType
+    {
+        public int PoojaCategoryTypeId { get; set; }
+        public string PoojaCategoryTypeName { get; set; }
+
+        public int CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
