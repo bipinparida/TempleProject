b6426de8cc8df6d0c5e7e7356722bd9631338c29
Trinatha kumar sethy <trinathsethy872@gmail.com>
2024-02-22 12:11:25 +0530

    PoojacategoryType crud operation

---
+++ AdminClient/Controllers/PoojaCategoryTypeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminClient.Controllers
+{
+    public class PoojaCategoryTypeController : Controller
+    {
+        public IActionResult CreatePoojaCategoryType()
+        {
+            return View();
+        }
+        public IActionResult PoojaCategoryTypeList()
+        {
+            return View();
+        }
+        public IActionResult PoojaCategoryTypeEdit(int PoojaCategoryTypeId)
+        {
+            ViewBag.PoojaCategoryTypeId = PoojaCategoryTypeId;
+            return View();
+        }
+    }
+}
