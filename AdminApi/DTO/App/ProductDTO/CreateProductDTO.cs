06816b1888fab6526a2200e0092d7f751ce4047a
Bipin Bihari Parida <bipinparida111@gmail.com>
2024-03-21 18:35:25 +0530

    Product Model Modify

--- AdminApi/DTO/App/ProductDTO/CreateProductDTO.cs
+++ AdminApi/DTO/App/ProductDTO/CreateProductDTO.cs
@@ -5,12 +5,14 @@
         public int? TempleId { get; set; }
         public int? PanditId { get; set; }
         public int CategoryId { get; set; }
-        public string ProductName { get; set; }
+		public int SubCategoryId { get; set; }
+		public string ProductName { get; set; }
         public double SalePrice { get; set; }
         public double MRP { get; set; }
         public double? DiscountAmount { get; set; }
         public string Thumbnail { get; set; }
-        public string Image1 { get; set; }
+		public string Image { get; set; }
+		public string Image1 { get; set; }
         public string Image2 { get; set; }
         public string Image3 { get; set; }
         public string Image4 { get; set; }
--- AdminApi/Models/App/Products/Product.cs
+++ AdminApi/Models/App/Products/Product.cs
@@ -8,13 +8,15 @@ namespace AdminApi.Models.App.Products
         public int? TempleId { get; set; }
         public int? PanditId { get; set; }
         public int CategoryId { get; set; }
-        public string ProductName { get; set; }
+		public int SubCategoryId { get; set; }
+		public string ProductName { get; set; }
         public double SalePrice {  get; set; }
         public double MRP { get; set; }
         public double? DiscountAmount { get; set; }
         public string Description { get; set; }
         public string Thumbnail { get; set; }
-        public string Image1 { get; set; }
+		public string Image { get; set; }
+		public string Image1 { get; set; }
         public string Image2 { get; set; }
         public string Image3 { get; set; }
         public string Image4 { get; set; }
