356deee2fb7f5af03370a2c91b870af4a46b6a70
Trinatha kumar sethy <trinathsethy872@gmail.com>
2024-04-04 11:07:05 +0530

    OrderList

---
+++ AdminClient/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminClient.Controllers
+{
+    public class OrderController : Controller
+    {
+        
+        public IActionResult OrderList()
+        {
+            return View();
+        }
+    }
+}
